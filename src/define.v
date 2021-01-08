1fb847bffd968952d24369114d5c6d3ef1591c08
Sree <58354729+sree-sankar@users.noreply.github.com>
2021-01-08 11:23:42 +0530

    Update define.v

--- src/define.v
+++ src/define.v
@@ -1,27 +1,6 @@
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.11.2020 12:07:34
-// Design Name: 
-// Module Name: define
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-//
-//
 
 `define XLEN 32
-`define ADDR_LEN 1024 
+`define ADDR_LEN 2**32 
 //OPCode[6:0]
 `define OP         7'b0110011 //Instruction : ADD,SUB,AND
 `define OP_IMM     7'b0010011 //Immediate Instruction : ADDI,SUBI,SULI
@@ -53,46 +32,31 @@
 `define FN3_XORI   3'b100 //XOR 
 `define FN3_ORI    3'b110 //OR 
 `define FN3_ANDI   3'b111 //AND 
-`define FN3_SLLI   3'b001 
-`define FN3_SRLI   3'b101 
-`define FN3_SRAI   3'b101 
+`define FN3_SLLI   3'b001 //Shift Left Logic
+`define FN3_SRLI   3'b101 //Shift Right Logic
+`define FN3_SRAI   3'b101 //Shift Right Arithmetic
 //Branch
 `define FN3_BEQ    3'b000 //Branch if Equal 
 `define FN3_BNE    3'b001 //Branch if Not Equal
 `define FN3_BLT    3'b100 //Branch if less than
 `define FN3_BGE    3'b101 //Branch if greater than
-`define FN3_BLTU   3'b110 //
-`define FN3_BGEU   3'b111 //
+`define FN3_BLTU   3'b110 //Branch if less than unsigned
+`define FN3_BGEU   3'b111 //Branch if greater than unsigned
 //Load
-`define FN3_LB     3'b000 //
-`define FN3_LH     3'b001 //
-`define FN3_LW     3'b010 //
-`define FN3_LBU    3'b100 //
-`define FN3_LHU    3'b101 //
+`define FN3_LB     3'b000 //Load Byte
+`define FN3_LH     3'b001 //Load Half Word
+`define FN3_LW     3'b010 //Load Word
+`define FN3_LBU    3'b100 //Load Byte Unsigned
+`define FN3_LHU    3'b101 //Load Half Unsigned
 //Store
-`define FN3_SB     3'b000 //
-`define FN3_SH     3'b001 //
-`define FN3_SW     3'b010 //
-`define FN3_SBU    3'b100 //
-`define FN3_SHU    3'b101 //
-//Load
+`define FN3_SB     3'b000 //Store Byte
+`define FN3_SH     3'b001 //Store Half Word
+`define FN3_SW     3'b010 //Store Word
+`define FN3_SBU    3'b100 //Store Byte Unsigned
+`define FN3_SHU    3'b101 //Store Half Unsigned
+//MISC-MEM
 `define FN3_FENCE  3'b000 //
 `define FN3_FENCEI 3'b001 //
-
-
 //-----------------Function 7 [31:25]---------------------------//
-//OP
 `define FN7_F1     7'b0000000 //ADD
 `define FN7_F2     7'b0100000 //SUB
-//`define FN7_SLL    7'b0000000 //Shift Left Logic
-//`define FN7_SLT    7'b0000000 //Compare signed
-//`define FN7_SLTU   7'b0000000 //Compare unsigned
-//`define FN7_XOR    7'b0000000 //XOR
-//`define FN7_SRL    7'b0000000 //Shift Right Logic
-//`define FN7_SRA    7'b0100000 //Shift Right Arithmetic
-//`define FN7_OR     7'b0000000 //OR
-//`define FN7_AND    7'b0000000 //AND
-////OP-IMM
-//`define FN7_SLLI   7'b0000000 //Shift Left Immediate
-//`define FN7_SRLI   7'b0000000 //Shift Right Immediate
-//`define FN7_SRAI   7'b0100000 //Shift Right Immediate
