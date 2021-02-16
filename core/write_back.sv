bb416a9f2cab4728111888cd536cfd61b9ed6af2
Sree Sankar E <58354729+sree-sankar@users.noreply.github.com>
2021-02-16 12:23:49 +0530

    Update write_back.sv

--- core/write_back.sv
+++ core/write_back.sv
@@ -1,17 +1,10 @@
-`timescale 1ns / 1ps
 `include"define.sv"
 
 module write_back(input  logic clk,halt,taken_branch,
-                  input  logic [`XLEN-1:0] instruction_in, alu_in,
+                  input  logic [`XLEN-1:0] instruction_in,alu_in, 
                   output logic reg_write_en);
                   
-   always_ff @(posedge clk)
-    begin
-        if(halt==1'b0 && taken_branch == 1'b0)
-        begin
-            reg_write_en   <= 1;
-            if(instruction_in[6:0] == `STORE)
-                 begin reg_write_en   <= 0; end
-            end
-    end
+          assign reg_write_en = (instruction_in[6:0] == `STORE ) ? 0 : 1;        
+                  
 endmodule:write_back
+
